c972f915a010df918bd72dec857d1d9ef30c4f41
David <h0195526@connect.hku.hk>
2020-02-23 21:26:40 +0800

    Updates

---
+++ precache-manifest.b9efc82976b73ab8f9b73ce4c5271360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364607d6fae7b36aafa5edff942701d0",
+    "url": "/handy-javascript/index.html"
+  },
+  {
+    "revision": "e3e8f129cfcb077337d3",
+    "url": "/handy-javascript/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e82ddedc211108d0cb70",
+    "url": "/handy-javascript/static/js/2.e8b47e5f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/handy-javascript/static/js/2.e8b47e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3e8f129cfcb077337d3",
+    "url": "/handy-javascript/static/js/main.443ac0ed.chunk.js"
+  },
+  {
+    "revision": "0bb0cc815c38dbc8779c",
+    "url": "/handy-javascript/static/js/runtime-main.76f7e598.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/handy-javascript/static/media/logo.5d5d9eef.svg"
+  }
+]);
