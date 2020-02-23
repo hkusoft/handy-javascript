7444ecb980a5305357b9c239e40fb9acbdb65fa9
David <h0195526@connect.hku.hk>
2020-02-23 21:50:19 +0800

    Add react placeholder project

---
+++ src/components/RepoCategories.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class RepoCategories extends Component {
+    render() {
+        return (
+            <div>
+                This is a category
+            </div>
+        );
+    }
+}
+
+export default RepoCategories;
